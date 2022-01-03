2c6704bd115859e9f9848456598b742ccac7e6fd
OlenaU <udalaolena@gmail.com>
2022-01-03 03:30:20 -0800

    font

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,19 +12,22 @@
         let tries = maxtries - count;
         console.log(tries);
         tryleft.innerHTML = (`${tries}`);
+        if (isNaN(x)) {
+            tryleft.innerHTML = (`Пожалуйста, введите число`);
+            return ;
+          }
     
           
         if (tries <= 0 ) 
         {
         tryleft.innerHTML = (`Ваши попытки закончились. Нажмите F5, чтобы сыграть снова.`);
-        console.log('все!');
         return;
         }
        
         else if (x == y)
         {    
         result.innerHTML = (`Ваш вариант ${x} является загаданным числом. Поздравяем Вас!`);  
-        result.innerHTML = (`Вы угадали его  за ${count} попыток. Нажмите F5, чтобы сыграть снова.`); 
+        result.innerHTML = (`Вы угадали число за ${count} попытки \\ попыток. Нажмите F5, чтобы сыграть снова.`); 
         return;
         
         }
